0256534c1123ca60b4067ca3959232d773216d49
Ian Burrett <ian@weestoater.com>
2024-05-12 10:39:38 +0100

    fixed broken tests

--- src/__tests__/patterns/appHeader.test.jsx
+++ src/__tests__/patterns/appHeader.test.jsx
@@ -1,23 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import { Header } from "../../patterns/appheader";
 
 describe("AppHeader pattern", () => {
   test("has weestoater branding", () => {
-    render(<Header />);
+    render(<MemoryRouter><Header /></MemoryRouter>);
     const branding = screen.getByText(/weestoater/i);
     expect(branding).toBeInTheDocument();
   });
 
   test("has weestoater icon", () => {
-    render(<Header />);
+    render(<MemoryRouter><Header /></MemoryRouter>);
     const altText = screen.getByAltText(/weestoater logo/i);
     expect(altText).toBeInTheDocument();
   });
 
   test("has github icon", () => {
-    render(<Header />);
+    render(<MemoryRouter><Header /></MemoryRouter>);
     const github = screen.getByTestId('github-icon');
     expect(github).toBeInTheDocument();
   });
